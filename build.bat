b65459dc9375ff8d91c44e3d1abd58eddbce1a10
samzmn <sam.zmn99@gmail.com>
2026-06-18 11:56:29 +0330

    dbs_labeling is in a separate file now

--- build.bat
+++ build.bat
@@ -1,12 +1,12 @@
-@echo off
+@echo on
 setlocal
 
 REM 1. Activate venv (adjust path if different)
 call .venv\Scripts\activate
 
 REM 2. Clean previous builds
-rmdir /s /q build
-rmdir /s /q dist
+rmdir /s /q "./build"
+rmdir /s /q "./dist/BrainViewer"
 del /q BrainViewer.spec
 
 REM 3. Run PyInstaller (spec if exists, otherwise generate)
@@ -16,11 +16,11 @@ if exist BrainViewer.spec (
     pyinstaller --name BrainViewer --windowed --onedir --paths src --add-data "src/resources;resources" src\main.py
 )
 
-REM 4. Package source zip
-powershell -Command "Compress-Archive -Path src/*, README.md, licenses/* -DestinationPath source_code.zip -Force"
+@REM REM 4. Package source zip
+@REM powershell -Command "Compress-Archive -Path src/*, README.md, licenses/* -DestinationPath source_code.zip -Force"
 
-REM 5. Build Inno Setup installer
-"C:\Program Files (x86)\Inno Setup 6\ISCC.exe" setup.iss
+@REM REM 5. Build Inno Setup installer
+@REM "C:\Program Files (x86)\Inno Setup 6\ISCC.exe" setup.iss
 
 echo Build complete.
 pause
